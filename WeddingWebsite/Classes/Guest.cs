ec4c270e5a16b0e112d1347bf0ddfff2221aea56
ElenaVonder <evonder13@gmail.com>
2021-03-13 17:11:08 -0500

    added class

---
+++ WeddingWebsite/Classes/Guest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingWebsite.Classes
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string  Lastname { get; set; }
+
+        public Guest()
+        {
+
+        }
+    }
+}
