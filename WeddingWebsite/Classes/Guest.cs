9d1059aa5271dce2bcfd225a635d920dda34df22
ElenaVonder <evonder13@gmail.com>
2021-03-16 10:44:01 -0400

    added parameters

--- WeddingWebsite/Classes/Guest.cs
+++ WeddingWebsite/Classes/Guest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,11 @@ namespace WeddingWebsite.Classes
     public class Guest
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength (30)]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Firstname { get; set; }
         public string  Lastname { get; set; }
 
