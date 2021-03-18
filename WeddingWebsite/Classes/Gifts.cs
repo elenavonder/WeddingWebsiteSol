77bfcee4604748c492e71d3edb4de07154972c0b
ElenaVonder <evonder13@gmail.com>
2021-03-17 21:03:42 -0400

    added new class

---
+++ WeddingWebsite/Classes/Gifts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingWebsite.Classes
+{
+    public class Gifts
+    {
+        public int Id { get; set; }
+
+
+        public Gifts()
+        {
+
+        }
+    }
+}
