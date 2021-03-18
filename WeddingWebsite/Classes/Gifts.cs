bdeb46b586b2a2f66fb679f373114dcd42812ec0
ElenaVonder <evonder13@gmail.com>
2021-03-17 21:09:14 -0400

    added properties

--- WeddingWebsite/Classes/Gifts.cs
+++ WeddingWebsite/Classes/Gifts.cs
@@ -8,7 +8,12 @@ namespace WeddingWebsite.Classes
     public class Gifts
     {
         public int Id { get; set; }
-
+        public string PartNbr { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Unit { get; set; }
+        public string PhotoPath { get; set; }
+        public int VendorId { get; set; }
 
         public Gifts()
         {
