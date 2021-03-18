9b39ba29f20b4d8d93e9f0b93e899ce041b56eac
ElenaVonder <evonder13@gmail.com>
2021-03-17 21:11:21 -0400

    added attributes

--- WeddingWebsite/Classes/Gifts.cs
+++ WeddingWebsite/Classes/Gifts.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,16 @@ namespace WeddingWebsite.Classes
     public class Gifts
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string PartNbr { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
+        [Column(TypeName ="decimal (11,2)")]
         public decimal Price { get; set; }
+        [Required]
+
         public string Unit { get; set; }
         public string PhotoPath { get; set; }
         public int VendorId { get; set; }
