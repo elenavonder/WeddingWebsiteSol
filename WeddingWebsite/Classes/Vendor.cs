6d3075ccce2f86420930e2ccf42b956723a4840b
ElenaVonder <evonder13@gmail.com>
2021-03-18 15:39:34 -0400

    added attributes

--- WeddingWebsite/Classes/Vendor.cs
+++ WeddingWebsite/Classes/Vendor.cs
@@ -12,12 +12,24 @@ namespace WeddingWebsite.Classes
         [Required]
         [StringLength(30)]
         public string Code { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Address { get; set; }
+        [Required]
+        [StringLength(30)]
         public string City { get; set; }
+        [Required]
+        [StringLength(2)]
         public string State { get; set; }
+        [Required]
+        [StringLength(5)]
         public string Zip { get; set; }
+        [StringLength(12)]
         public string Phone { get; set; }
+        [StringLength(255)]
         public string Email { get; set; }
 
         public Vendor()
