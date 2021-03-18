324c4b4c95cbfe2389623f8a970a456475087236
ElenaVonder <evonder13@gmail.com>
2021-03-18 15:37:52 -0400

    added properties

---
+++ WeddingWebsite/Classes/Vendor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingWebsite.Classes
+{
+    public class Vendor
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public Vendor()
+        {
+
+        }
+    }
+}
