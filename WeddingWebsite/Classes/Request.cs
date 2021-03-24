9142e87935751f6bd59b3d821c92908119881356
ElenaVonder <evonder13@gmail.com>
2021-03-23 20:36:21 -0400

    added class

---
+++ WeddingWebsite/Classes/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingWebsite.Classes
+{
+    public class Request
+    {
+
+        public Request()
+        {
+
+        }
+    }
+}
