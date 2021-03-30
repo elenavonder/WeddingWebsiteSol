68de58e9179e43f9e56b6dedd272657f711e0f05
ElenaVonder <evonder13@gmail.com>
2021-03-30 12:48:39 -0400

    fixed bugs

--- WeddingWebsite/Classes/Request.cs
+++ WeddingWebsite/Classes/Request.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,8 +22,9 @@ namespace WeddingWebsite.Classes
         [StringLength(20)]
         public string DevliveryMode { get; set; } = "pickup";
         [Required]
-
+        [StringLength(30)]
         public string Status { get; set; } = "New";
+        [Column(TypeName = "decimal(11,2)")]
         public decimal Total { get; set; } = 0;
         public int GuestId { get; set; }
         public virtual Guest Guest { get; set; }
